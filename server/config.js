7c9143a57a1136f3a928b2c257daec78a5c864c3
Ahm A <asad.gooogle@gmail.com>
2018-03-14 00:44:24 -0400

    Update config.js

--- server/config.js
+++ server/config.js
@@ -3,14 +3,5 @@ const day = 24 * 60 * 60 * 1000;
 module.exports = {
   server: {
     port: process.env.PORT || 3000
-  },
-  auth: {
-    session: {
-      expiry: process.env.CHAT_APP_AUTH_SESSION_EXPIRY || (3 * day)
-    },
-    cookie: {
-      password: process.env.CHAT_APP_AUTH_COOKIE_PASSWORD || 'password-must-be-at-least-32-characters',
-      isSecure: false
-    }
   }
 }
